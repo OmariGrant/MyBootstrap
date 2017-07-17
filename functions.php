2047395e64b3cd34bebe3d8bdedebebe4dfdfd3a
OmariGrant <ogrant92@gmail.com>
2017-07-17 01:13:01 +0100

    add google font

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ function addBootstrap() {
  wp_enqueue_style( 'mainCSS', get_template_directory_uri() . '/assets/css/custom.css' );
      wp_enqueue_script( 'jQuery', get_template_directory_uri() . '/assets/js/jquery.min.js');
 	wp_enqueue_script( 'bootstrapJS', get_template_directory_uri() . '/assets/js/bootstrap.min.js');
-	wp_enqueue_style( 'googleFont', 'https://fonts.googleapis.com/css?family=Aldrich');	    
+	//wp_enqueue_style( 'googleFont', 'https://fonts.googleapis.com/css?family=Aldrich');	    
 
 
 }
